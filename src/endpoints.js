8a57f585a6930917bbe85211dd6ba18d5a23ad5d
Kalel <ZoroPlay1220@gmail.com>
2022-05-24 17:37:42 -0300

    start api

---
+++ src/endpoints.js
@@ -0,0 +1,151 @@
+import { Router } from 'express'
+import { somar, dobro, media, temp, tabuada, text, cinema, corprimaria, maiornumero } from './services.js'
+const server = Router();
+
+server.get('/dobro/:numero', (req, resp) => {
+    try{const numero = Number(req.params.numero);
+        const b = dobro(numero)
+        resp.send({
+            dobro: b
+        })
+    }
+    catch(err){
+        resp.status(500).send({
+            error: err.message 
+        })
+    }
+})
+server.get('/somar', (req,resp) => {
+    try{
+    const a = Number(req.query.a);
+    const b = Number(req.query.b);
+    const x = somar(a, b)
+    resp.send({
+        soma: x
+    })
+    }
+    catch(err){
+        resp.status(500).send({
+            error: err.message
+        })
+    }
+})
+
+
+server.post('/somar', (req, resp) => {
+    try{
+        const { a: num1, b: num2 } = req.body;
+    const soma = somar(num1, num2);
+    resp.send({
+        soma: soma
+    })
+    }
+    catch(err){
+        resp.status(500).send({
+            error: err.message
+        })
+    }
+})
+server.post('/media', (req, resp) => {
+    try{
+        const { a: num1, b: num2, c: num3 } = req.body;
+    const resposta = media(num1, num2, num3);
+    resp.send({
+        media: resposta
+    })
+    }
+    catch(err){
+        resp.status(500).send({
+            error: err.message
+        })
+    }
+})
+
+server.get('/temp', (req,resp) => {
+    try{
+    const a = Number(req.query.a);
+    const x = temp(a)
+    resp.send({
+        febre: x
+    })
+    }
+    catch(err){
+        resp.status(500).send({
+            error: err.message
+        })
+    }
+})
+
+server.get('/tab', (req,resp) => {
+    try{
+    const a = Number(req.query.a);
+    const x = tabuada(a)
+    resp.send(x)
+    }
+    catch(err){
+        resp.status(500).send({
+            error: err.message
+        })
+    }
+})
+server.get('/dia2/freqcaracter/:texto/:caracter', (req,resp) => {
+    try{
+    const a = String(req.params.texto);
+    const b = String(req.params.caracter)
+
+    const x = text(a, b)
+    resp.send({
+        freq: x
+    })
+    }
+    catch(err){
+        resp.status(500).send({
+            error: err.message
+        })
+    }
+})
+server.get('/dia2/corprimaria/:cor', (req,resp) => {
+    try{
+    const a = String(req.params.cor);
+    const x = corprimaria(a)
+    resp.send({
+        resposta: x
+    })
+    }
+    catch(err){
+        resp.status(500).send({
+            error: err.message
+        })
+    }
+})
+
+server.post('/dia2/ingressocinema', (req, resp) => {
+    try{
+    const { a: num1, b: num2, c: num3, d:num4 } = req.body;
+    const resposta = cinema(num1, num2, num3, num4);
+    resp.send({
+        total: resposta
+    })
+    }
+    catch(err){
+        resp.status(500).send({
+            error: err.message
+        })
+    }
+})
+
+server.post('/dia2/maiornumero', (req, resp) => {
+    try{
+        const { a: num1 } = req.body;
+    const resposta = media(num1);
+    resp.send({
+        maior: resposta
+    })
+    }
+    catch(err){
+        resp.status(500).send({
+            error: err.message
+        })
+    }
+})
+export default server
---
+++ src/services.js
@@ -0,0 +1,76 @@
+function  dobro(n){
+    return n * 2;
+}
+function somar(a, b){
+    return a + b;
+}
+function media(a,b,c){
+    return (a + b + c) /3;
+}
+
+function temp(t){
+    if(t >= 37){
+        return true
+    }
+    else{
+        return false
+    }
+}
+
+function tabuada(n){
+    let array = [];
+
+    for(let i = 0; i <= 10; i++){
+        array[i] = i * n; 
+    }
+    return array;
+}
+
+// get
+function text(t, c){
+    let e = 0
+    for(let letter of t){
+
+        if(letter == c){
+            e++
+        }
+    }
+    return e;
+}
+
+function cinema(inteira, meia, dia, nacao){
+
+    let mei = 14.25 * meia
+    let int = 28.5 * inteira
+    if(nacao == 'brasileira'){
+        int = 5 * inteira
+        mei = 5 * meia
+        return int + mei;
+    }
+    if(dia == 'quarta'){
+        int = 14.25 * inteira
+        return int + mei
+    }
+}
+
+// get
+function corprimaria(cor){
+    let respo = false
+    if(cor == "amarelo" || cor == "azul" || cor == "vermelho"){
+        respo = true;
+    }
+    return respo
+    
+}
+
+function maiornumero(array){
+    let maior = 0;
+    for(let item of array){
+        if(item > maior){
+            maior = item;
+        }
+    }
+    return maior;
+}
+
+export {somar, dobro, media, temp, tabuada, text, cinema, corprimaria, maiornumero}
